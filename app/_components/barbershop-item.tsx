a42e5c790059332577109a3acd2e35ba05e8db3b
Matheus Felipe dos Santos <matheusfelipe685@gmail.com>
2024-08-27 20:40:08 -0300

    feat: ensure booking can be created

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,3 +1,5 @@
+// TODO: Pagina completa
+
 import { Barbershop } from "@prisma/client"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+// TODO: Pagina completa
+
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
@@ -7,23 +9,40 @@ import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
+import { getConfirmedBookings } from "./_data/get-confirmed-bookings"
 
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
-  const pupolarBarbershops = await db.barbershop.findMany({
+  const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+  const confirmedBookings = await getConfirmedBookings()
 
   return (
     <div>
-      {/* Header*/}
+      {/* header */}
       <Header />
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="fron-bold text-xl">Olá, Matheus!</h2>
-        <p>Domingo, 11 de agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
@@ -62,8 +81,23 @@ const Home = async () => {
           />
         </div>
 
-        {/* AGENDAMENTO */}
-        <BookingItem />
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Agendamentos
+            </h2>
+
+            {/* AGENDAMENTO */}
+            <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+              {confirmedBookings.map((booking) => (
+                <BookingItem
+                  key={booking.id}
+                  booking={JSON.parse(JSON.stringify(booking))}
+                />
+              ))}
+            </div>
+          </>
+        )}
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
@@ -78,7 +112,7 @@ const Home = async () => {
           Populares
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-          {pupolarBarbershops.map((barbershop) => (
+          {popularBarbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
